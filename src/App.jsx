aef49ccf0a24496e0e92352aa90ddcf632cd850e
firat <firat.duzgun@hotmail.com>
2024-11-23 14:39:52 +0300

    try tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
-import { useState } from 'react';
-import PersonalInput from './PersonalInput';
+// import { useState } from 'react';
+// import Personal from './Personal';
+// import Education from './Education';
+// import Experience from './Experience';
+// import Template from './Template';
 
 const App = () => {
-  const [personal, setPersonal] = useState({
-    name: 'Damla OZ',
-    mail: 'tahmin@baka.lim',
-    phone: '+90123456',
-    location: 'Bursa, Turkiye',
-  });
-  const [education, setEducation] = useState([
-    {
-      school: 'Mimar Sinan University',
-      degree: 'Sanat ve Sepet Isleri',
-      startDate: '01.01.2001',
-      endDate: '02.02.2002',
-      id: crypto.randomUUID(),
-    },
-  ]);
-  const [experience, setExperience] = useState([
-    {
-      company: 'Random Grafik Tasarim Atolyesi',
-      title: 'Grafik Tasarim Bacisi',
-      startDate: '03.03.2003',
-      endDate: '04.04.2004',
-      description: '',
-      id: crypto.randomUUID(),
-    },
-  ]);
+  // const [personal, setPersonal] = useState({
+  //   name: 'Damla OZ',
+  //   mail: 'tahmin@baka.lim',
+  //   phone: '+90123456',
+  //   location: 'Bursa, Turkiye',
+  // });
+  // const [education, setEducation] = useState([
+  //   {
+  //     school: 'Mimar Sinan University',
+  //     degree: 'Sanat ve Sepet Isleri',
+  //     startDate: '01.01.2001',
+  //     endDate: '02.02.2002',
+  //     id: crypto.randomUUID(),
+  //   },
+  // ]);
+  // const [experience, setExperience] = useState([
+  //   {
+  //     company: 'Random Grafik Tasarim Atolyesi',
+  //     title: 'Grafik Tasarim Bacisi',
+  //     startDate: '03.03.2003',
+  //     endDate: '04.04.2004',
+  //     description: '',
+  //     id: crypto.randomUUID(),
+  //   },
+  // ]);
 
-  const handleInputName = () => {
-    let update = document.querySelector('#inputName');
-    setPersonal({ ...personal, name: update.value });
-  };
-  const [isActive, setIsActive] = useState(false);
+  // const handleInputName = () => {
+  //   let update = document.querySelector('#inputName');
+  //   setPersonal({ ...personal, name: update.value });
+  // };
+  // const [isActive, setIsActive] = useState(false);
 
   return (
     <>
-      <PersonalInput data={personal} fetchName={handleInputName} />
+      <div className="bg-gradient-to-r from-gray-300 to-gray-600">
+        <h1>Hi there</h1>
+      </div>
     </>
   );
 };
